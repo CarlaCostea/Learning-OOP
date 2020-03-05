47c204e381a927c1eea3e1c70b826b5968b04760
CarlaCostea <CarlaCostea@users.noreply.github.com>
2020-03-05 16:31:48 +0200

    Update StudentClassBookTests.cs

--- ClassBook.Tests/StudentClassBookTests.cs
+++ ClassBook.Tests/StudentClassBookTests.cs
@@ -7,16 +7,6 @@ namespace ClassBook.Tests
 {
     public class StudentClassBookTests
     {
-        [Fact]
-        public void AddStudentInClassBookTest()
-        {
-            Student student = new Student("Maria");
-            StudentsClassBook catalog = new StudentsClassBook();
-
-            catalog.AddStudent(student);
-
-            Assert.True(catalog.VerifyStudentInList("Maria"));
-        }
 
         [Fact]
         public void ReturnPozitionOfStudentInSortedList2Students()
