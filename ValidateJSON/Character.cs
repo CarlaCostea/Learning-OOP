0ec720b5eaca98ee329c3e29cbdab9f5aeb530ae
CarlaCostea <CarlaCostea@users.noreply.github.com>
2020-03-09 15:50:35 +0200

    Update Character.cs

--- ValidateJSON/Character.cs
+++ ValidateJSON/Character.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace ValidateJSON
 {
-    class Class1
+    public class Character
     {
+        readonly char pattern;
+
+        public Character(char pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public bool Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return false;
+            }
+
+            return text[0] == pattern;
+        }
     }
 }
