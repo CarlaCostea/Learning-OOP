0e8cba8d2375080eede7def7588b0be108595157
CarlaCostea <CarlaCostea@users.noreply.github.com>
2020-03-09 15:25:56 +0200

    red
    https://www.juniormind.com/content/dump/abstraction.html

---
+++ ValidateJSON.tests/CharacterTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ValidateJSON.tests
+{
+    public class CharacterTests
+    {
+        [Fact]
+        public void IfStringIsNullWeShouldReturnFalse()
+        {
+            var x = new Character('x');
+            Assert.False(x.Match(null));
+        }
+
+        [Fact]
+        public void IfStringIsEmptyWeShouldReturnFalse()
+        {
+            var x = new Character('x');
+            Assert.False(x.Match(""));
+        }
+
+        [Fact]
+        public void IfFirstLetterIsInStringWeShouldReturnTrue()
+        {
+            var x = new Character('x');
+            Assert.True(x.Match("xtg"));
+        }
+
+        [Fact]
+        public void IfFirstLetterIsNotInStringWeShouldReturnTrue()
+        {
+            var x = new Character('x');
+            Assert.False(x.Match("wtg"));
+        }
+    }
+}
