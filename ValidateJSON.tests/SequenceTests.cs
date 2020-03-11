80fe67b67efba642b9dde66bda768d03f9959614
CarlaCostea <CarlaCostea@users.noreply.github.com>
2020-03-11 12:40:05 +0200

    Update SequenceTests.cs

--- ValidateJSON.tests/SequenceTests.cs
+++ ValidateJSON.tests/SequenceTests.cs
@@ -54,5 +54,133 @@ namespace ValidateJSON.tests
             Assert.False(match.Success());
             Assert.Equal("", match.RemainingText());
         }
+
+        [Fact]
+        public void SequenceOfMatchingSequencesShouldReturnTrue()
+        {
+            var ab = new Sequence(
+            new Character('a'),
+            new Character('b')
+        );
+            var abc = new Sequence(
+                ab,
+                new Character('c')
+            );
+            var match = abc.Match("abcd");
+            Assert.True(match.Success());
+            Assert.Equal("d", match.RemainingText());
+        }
+
+        [Fact]
+        public void SequenceOfNotMatchingSequencesShouldReturnFalse()
+        {
+            var ab = new Sequence(
+            new Character('a'),
+            new Character('b')
+        );
+            var abc = new Sequence(
+                ab,
+                new Character('c')
+            );
+            var match = abc.Match("abx");
+            Assert.False(match.Success());
+            Assert.Equal("abx", match.RemainingText());
+        }
+
+        [Fact]
+        public void SequenceOfMatchingRepeteadSequencesShouldReturnTrue()
+        {
+            var hex = new Choice(
+                new Range('0', '9'),
+            
+                new Range('a', 'f'),
+                new Range('A', 'F')
+            );
+
+            var hexSeq = new Sequence(
+                new Character('u'),
+                new Sequence(
+                    hex,
+                    hex,
+                    hex,
+                    hex
+                )
+            );
+            var match = hexSeq.Match("u1234");
+            Assert.True(match.Success());
+            Assert.Equal("", match.RemainingText());
+        }
+
+        [Fact]
+        public void SequenceOfMatchingRepeteadSequencesShouldReturnTrue1()
+        {
+            var hex = new Choice(
+                new Range('0', '9'),
+
+                new Range('a', 'f'),
+                new Range('A', 'F')
+            );
+
+            var hexSeq = new Sequence(
+                new Character('u'),
+                new Sequence(
+                    hex,
+                    hex,
+                    hex,
+                    hex
+                )
+            );
+            var match = hexSeq.Match("uabcdef");
+            Assert.True(match.Success());
+            Assert.Equal("ef", match.RemainingText());
+        }
+
+        [Fact]
+        public void SequenceOfMatchingRepeteadSequencesShouldReturnTrue2()
+        {
+            var hex = new Choice(
+                new Range('0', '9'),
+
+                new Range('a', 'f'),
+                new Range('A', 'F')
+            );
+
+            var hexSeq = new Sequence(
+                new Character('u'),
+                new Sequence(
+                    hex,
+                    hex,
+                    hex,
+                    hex
+                )
+            );
+            var match = hexSeq.Match("uB005 ab");
+            Assert.True(match.Success());
+            Assert.Equal(" ab", match.RemainingText());
+        }
+
+        [Fact]
+        public void SequenceOfNotMatchingRepeteadSequencesShouldReturnFalse()
+        {
+            var hex = new Choice(
+                new Range('0', '9'),
+
+                new Range('a', 'f'),
+                new Range('A', 'F')
+            );
+
+            var hexSeq = new Sequence(
+                new Character('u'),
+                new Sequence(
+                    hex,
+                    hex,
+                    hex,
+                    hex
+                )
+            );
+            var match = hexSeq.Match("abc");
+            Assert.False(match.Success());
+            Assert.Equal("abc", match.RemainingText());
+        }
     }
 }
