ef157ff98c3d7c488e82f2632be57813946f58a4
CarlaCostea <CarlaCostea@users.noreply.github.com>
2020-03-11 12:24:32 +0200

    Sequence
    https://www.juniormind.com/content/dump/sequence.html

---
+++ ValidateJSON.tests/SequenceTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ValidateJSON.tests
+{
+    public class SequenceTests
+    {
+        [Fact]
+        public void SequenceOfMatchingCharactersShoudReturnTrue()
+        {
+            var ab = new Sequence(
+                new Character('a'),
+                new Character('b')
+            );
+            var match = ab.Match("abcd");
+            Assert.True(match.Success());
+            Assert.Equal("cd", match.RemainingText());
+        }
+
+        [Fact]
+        public void SequenceOfNotMatchingCharactersShoudReturnFalse()
+        {
+            var ab = new Sequence(
+                new Character('a'),
+                new Character('x')
+            );
+            var match = ab.Match("abcd");
+            Assert.False(match.Success());
+            Assert.Equal("abcd", match.RemainingText());
+        }
+    }
+}
---
+++ ValidateJSON/Sequence.cs
@@ -0,0 +1,29 @@
+﻿namespace ValidateJSON
+{
+    public class Sequence : IPattern
+    {
+        private readonly IPattern[] patterns;
+
+        public Sequence(params IPattern[] patterns)
+        {
+            this.patterns = patterns;
+        }
+
+        public IMatch Match(string text)
+        {
+            string initialText = text;
+            foreach (var pattern in patterns)
+            {
+                var match = pattern.Match(text);
+                if (!match.Success())
+                {
+                    return new Match(initialText, false);
+                }
+
+                text = match.RemainingText();
+            }
+
+            return new Match(text, true);
+        }
+    }
+}
