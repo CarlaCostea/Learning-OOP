f3c23e20d41fa21439101214183f02145afe2410
CarlaCostea <CarlaCostea@users.noreply.github.com>
2020-03-13 17:07:41 +0200

    Optional
    https://juniormind.com/content/dump/optional.html

---
+++ ValidateJSON.tests/OptionalTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ValidateJSON.tests
+{
+    public class OptionalTests
+    {
+        [Fact]
+        public void ConsumePatternIfIsFoundAtTheBegginingOfTheString()
+        {
+            var a = new Optional(new Character('a'));
+            var match = a.Match("abc");
+            Assert.True(match.Success());
+            Assert.Equal("bc", match.RemainingText());
+        }
+    }
+}
---
+++ ValidateJSON/Optional.cs
@@ -0,0 +1,17 @@
+﻿namespace ValidateJSON
+{
+    public class Optional : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public Optional(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            return null;
+        }
+    }
+}
