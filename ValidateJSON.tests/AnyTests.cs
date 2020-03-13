9485ea1be66788c2055355076727115ebfd20c58
CarlaCostea <CarlaCostea@users.noreply.github.com>
2020-03-13 11:10:38 +0200

    any
    https://juniormind.com/content/dump/any.html

---
+++ ValidateJSON.tests/AnyTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ValidateJSON.tests
+{
+    public class AnyTests
+    {
+        [Fact]
+        public void IfFirstLetterIsInStringWeShouldReturnTrue()
+        {
+            var e = new Any("eE");
+            var match = e.Match("ea");
+            Assert.True(match.Success());
+            Assert.Equal("a", match.RemainingText());
+        }
+    }
+}
---
+++ ValidateJSON/Any.cs
@@ -0,0 +1,17 @@
+﻿namespace ValidateJSON
+{
+    public class Any : IPattern
+    {
+        private readonly string accepted;
+
+        public Any(string accepted)
+        {
+            this.accepted = accepted;
+        }
+
+        public IMatch Match(string text)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
