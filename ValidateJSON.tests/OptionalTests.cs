32acba31dbe8b9b7bb772b89580382b130da1d93
CarlaCostea <CarlaCostea@users.noreply.github.com>
2020-03-18 11:12:07 +0200

    Update OptionalTests.cs

--- ValidateJSON.tests/OptionalTests.cs
+++ ValidateJSON.tests/OptionalTests.cs
@@ -15,5 +15,41 @@ namespace ValidateJSON.tests
             Assert.True(match.Success());
             Assert.Equal("bc", match.RemainingText());
         }
+
+        [Fact]
+        public void ConsumePatternIfIsFoundAtTheBegginingOfTheString1()
+        {
+            var a = new Optional(new Character('a'));
+            var match = a.Match("aabc");
+            Assert.True(match.Success());
+            Assert.Equal("abc", match.RemainingText());
+        }
+
+        [Fact]
+        public void ReturnTrueAndTheStringIfPatternIsNotFound()
+        {
+            var a = new Optional(new Character('a'));
+            var match = a.Match("bc");
+            Assert.True(match.Success());
+            Assert.Equal("bc", match.RemainingText());
+        }
+
+        [Fact]
+        public void ReturnTrueForEmptyString()
+        {
+            var a = new Optional(new Character('a'));
+            var match = a.Match("");
+            Assert.True(match.Success());
+            Assert.Equal("", match.RemainingText());
+        }
+
+        [Fact]
+        public void ReturnTrueForNull()
+        {
+            var a = new Optional(new Character('a'));
+            var match = a.Match(null);
+            Assert.True(match.Success());
+            Assert.Null(match.RemainingText());
+        }
     }
 }
