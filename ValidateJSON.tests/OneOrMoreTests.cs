6d92684a9cabcd50d4220ba56d3c7490f21baff8
CarlaCostea <CarlaCostea@users.noreply.github.com>
2020-03-18 12:05:29 +0200

    OneOrMore
    https://juniormind.com/content/dump/oneormore.html

---
+++ ValidateJSON.tests/OneOrMoreTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ValidateJSON.tests
+{
+    public class OneOrMoreTests
+    {
+        [Fact]
+        public void ConsumePatternIfIsFoundAtLeastOneTimeInString()
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+            var match = a.Match("123");
+            Assert.True(match.Success());
+            Assert.Equal("", match.RemainingText());
+        }
+    }
+}
---
+++ ValidateJSON/OneOrMore.cs
@@ -0,0 +1,19 @@
+﻿namespace ValidateJSON
+{
+    public class OneOrMore : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public OneOrMore(IPattern pattern)
+        {
+            // aici folosește-te de clasele implementate deja
+            // pentru a construi un pattern care să îl folosești în Match
+            this.pattern = new Many(pattern);
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
