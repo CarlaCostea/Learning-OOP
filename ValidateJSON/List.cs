29ec713aab3c731832b5b2a0d0bfa77a4e0d6d5c
CarlaCostea <CarlaCostea@users.noreply.github.com>
2020-03-20 16:44:13 +0200

    Update List.cs

--- ValidateJSON/List.cs
+++ ValidateJSON/List.cs
@@ -6,7 +6,7 @@
 
         public List(IPattern element, IPattern separator)
         {
-            this.pattern = new Many(new Sequence(element, new Many(new Sequence(new Optional(element), new Optional(separator), new Optional(element)))));
+            this.pattern = new Optional(new Sequence(element, new Many(new Sequence(separator, element))));
         }
 
         public IMatch Match(string text)
