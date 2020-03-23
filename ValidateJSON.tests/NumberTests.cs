a2c585551bab054f150f954779de8d2b984e2785
CarlaCostea <CarlaCostea@users.noreply.github.com>
2020-03-23 12:09:19 +0200

    Number Class
    https://juniormind.com/content/dump/json.html

---
+++ ValidateJSON.tests/NumberTests.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ValidateJSON.tests
+{
+    public class NumberTests
+    {
+        [Fact]
+        public void CorrectFloatNumberShouldReturnTrue3()
+        {
+            string input = "12.12";
+            Assert.True(ValidateJSONNumber.ValidateInput(input));
+        }
+
+        [Fact]
+        public void CorrectFloatNumberShouldReturnTrue4()
+        {
+            string input = "-12.12";
+            Assert.True(ValidateJSONNumber.ValidateInput(input));
+        }
+        [Fact]
+        public void SubunitarShouldReturnTrue()
+        {
+            string input = "0.1312";
+            Assert.True(ValidateJSONNumber.ValidateInput(input));
+        }
+        [Fact]
+        public void NegativeSubunitarShouldReturnTrue()
+        {
+            string input = "-0.1312";
+            Assert.True(ValidateJSONNumber.ValidateInput(input));
+        }
+        [Fact]
+        public void TreatsZeroCorecttly()
+        {
+            var number = new Number();
+            Assert.True(number.Match("0").Success());
+            Assert.False(number.Match("00").Success());
+        }
+        [Fact]
+        public void NumberPreceededByZeroShouldReturnFalse()
+        {
+            var number = new Number();
+            Assert.False(number.Match("01").Success());
+        }
+        [Fact]
+        public void OneDigitShouldReturnTrue()
+        {
+            var number = new Number();
+            Assert.True(number.Match("1").Success());
+        }
+        [Fact]
+        public void NaturalsShouldReturnTrue()
+        {
+            var number = new Number();
+            Assert.True(number.Match("6701").Success());
+        }
+        [Fact]
+        public void NegativeShouldReturnTrue()
+        {
+            var number = new Number();
+            Assert.True(number.Match("-6701").Success());
+        }
+        [Fact]
+        public void NegativeDigitShouldReturnTrue()
+        {
+            var number = new Number();
+            Assert.True(number.Match("-1").Success());
+        }
+        [Fact]
+        public void ExponentialShouldReturnTrue()
+        {
+            string input = "12.123e3";
+            Assert.True(ValidateJSONNumber.ValidateInput(input));
+        }
+        [Fact]
+        public void CorrectExponentialFormatShouldReturnTrue1()
+        {
+            string input = "12.123E+3";
+            Assert.True(ValidateJSONNumber.ValidateInput(input));
+        }
+        [Fact]
+        public void CorrectExponentialFormatShouldReturnTrue2()
+        {
+            string input = "12.123E-2";
+            Assert.True(ValidateJSONNumber.ValidateInput(input));
+        }
+        [Fact]
+        public void InvalidExponentialFormatShouldReturnFalse()
+        {
+            string input = "12.123E";
+            Assert.False(ValidateJSONNumber.ValidateInput(input));
+        }
+        [Fact]
+        public void InvalidExponentialFormatShouldReturnFalse1()
+        {
+            string input = "12.123E+";
+            Assert.False(ValidateJSONNumber.ValidateInput(input));
+        }
+        [Fact]
+        public void InvalidExponentialFormatShouldReturnFalse2()
+        {
+            string input = "12.123E+.";
+            Assert.False(ValidateJSONNumber.ValidateInput(input));
+        }
+    }
+}
---
+++ ValidateJSON/Number.cs
@@ -0,0 +1,23 @@
+﻿namespace ValidateJSON
+{
+    public class Number : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public Number()
+        {
+            var onenine = new Range('1', '9');
+            var zero = new Character('0');
+            var digits = new Choice(zero, onenine);
+            var natural = new Sequence(onenine, new Many(digits));
+            var integer = new Sequence(new Optional(new Character('-')), natural);
+            var fractional = new Sequence(new Character('.'), natural);
+            this.pattern = new Sequence(new Choice(integer, zero), new Optional(fractional));
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
