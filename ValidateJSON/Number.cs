f668efd3158b92dfc2f0fd518e17693b88132d08
CarlaCostea <CarlaCostea@users.noreply.github.com>
2020-03-24 18:03:06 +0200

    Update Number.cs

--- ValidateJSON/Number.cs
+++ ValidateJSON/Number.cs
@@ -11,8 +11,8 @@
             var digit = new Range('0', '9');
             var digits = new OneOrMore(digit);
             var sign = new Optional(new Any("+-"));
-            var natural = new Sequence(onenine, new Many(digit));
-            var integer = new Choice(zero, new Sequence(new Optional(new Character('-')), natural));
+            var natural = new Sequence(new Choice(zero, new Sequence(onenine, new Many(digit))));
+            var integer = new Sequence(new Optional(new Character('-')), natural);
             var fractional = new Sequence(new Character('.'), digits);
             var exponential = new Sequence(new Any("Ee"), new Optional(sign), digits);
             pattern = new Sequence(integer, new Optional(fractional), new Optional(exponential));
