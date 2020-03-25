5f1ec7ec7eae96381c5bd8be22643ea5432f770a
CarlaCostea <CarlaCostea@users.noreply.github.com>
2020-03-25 09:53:12 +0200

    update

---
+++ ValidateJSON/String.cs
@@ -0,0 +1,17 @@
+﻿namespace ValidateJSON
+{
+    public class String : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public String()
+        {
+            pattern = new Many(pattern);
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
