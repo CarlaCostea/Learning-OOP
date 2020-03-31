c5192ec0ac54b9bd6f471dfa52150b1ecbcae4d4
CarlaCostea <CarlaCostea@users.noreply.github.com>
2020-03-31 16:34:01 +0300

    Update Value.cs

--- ValidateJSON/Value.cs
+++ ValidateJSON/Value.cs
@@ -6,7 +6,18 @@
 
             public Value()
             {
-                pattern = new Many(new Number());
+            var number = new Number();
+            var @string = new String();
+            var ws = new Choice(new Character('\u0020'), new Character('\u000A'), new Character('\u000D'), new Character('\u0009'));
+            var value = new Value();
+            var element = new List(value, ws);
+            var elements = new Choice(element, new Sequence(element, new Character(','), new OneOrMore(element)));
+            pattern = new Choice(
+                        @string,
+                        number,
+                        new Text("true"),
+                        new Text("false"),
+                        new Text("null"));
             }
 
             public IMatch Match(string text)
