5e09c6b5ca103f8bc3e4db44af7a042bdf97486a
CarlaCostea <CarlaCostea@users.noreply.github.com>
2020-04-18 10:48:24 +0300

    Update SortedIntArray.cs

--- ArrayOperations/SortedIntArray.cs
+++ ArrayOperations/SortedIntArray.cs
@@ -6,6 +6,32 @@
         {
         }
 
+        public override int this[int index]
+        {
+            get => this[index];
+            set
+                {
+                if (index == 0 && value <= this[0])
+                {
+                    this[index] = value;
+                    return;
+                }
+
+                if (index == Count - 1 && value >= this[Count - 1])
+                {
+                    this[index] = value;
+                    return;
+                }
+
+                if (index <= 0 || index >= Count - 1 || this[index] < value)
+                {
+                    return;
+                }
+
+                this[index] = value;
+            }
+        }
+
         public override void Add(int element)
         {
             if (Count == 0)
