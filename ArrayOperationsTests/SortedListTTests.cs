155251fa2c84b7042bc08659485777ea3c53b7c9
CarlaCostea <CarlaCostea@users.noreply.github.com>
2020-04-28 18:10:38 +0300

    Update SortedListTTests.cs

--- ArrayOperationsTests/SortedListTTests.cs
+++ ArrayOperationsTests/SortedListTTests.cs
@@ -65,5 +65,17 @@ namespace ArrayOperationsTests
             Assert.Equal(0, sortedList.IndexOf(1));
             Assert.Equal(9, sortedList[4]);
         }
+
+        [Fact]
+        public void AddIntElementsAndTestEnumerator()
+        {
+            var intArray = new SortedListT<int>() { 5, 3, 7, 1, 9 };
+            Assert.Equal(5, intArray.Count);
+            Assert.False(intArray.Contains(2));
+            Assert.Equal(-1, intArray.IndexOf(2));
+            var enumerate = intArray.GetEnumerator();
+            Assert.True(enumerate.MoveNext());
+            Assert.Equal(1, enumerate.Current);
+        }
     }
 }
