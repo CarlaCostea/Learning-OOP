d5b8957683e2cb16b2e46832dc772a537bef1d00
CarlaCostea <CarlaCostea@users.noreply.github.com>
2020-05-05 10:21:03 +0300

    Update ReadOnlyColletctionTests.cs

--- ArrayOperationsTests/ReadOnlyColletctionTests.cs
+++ ArrayOperationsTests/ReadOnlyColletctionTests.cs
@@ -27,6 +27,40 @@ namespace ArrayOperationsTests
         }
 
         [Fact]
+
+        public void TryingToAddAnElementInReadonlyListShoulTrowAnError()
+        {
+            var list = new ListT<int> { 0, 1, 2, 3 };
+            ReadOnlyCollection<int> readOnlyList = new ReadOnlyCollection<int>(list);
+            Assert.Equal(new[] { 0, 1, 2, 3 }, readOnlyList);
+            Assert.Throws<NotSupportedException>(() => readOnlyList.Add(4));
+            Assert.Equal(new[] { 0, 1, 2, 3 }, readOnlyList);
+        }
+
+        [Fact]
+
+        public void TryingToRemoveAnElementFromReadonlyListShoulTrowAnError()
+        {
+            var list = new ListT<int> { 0, 1, 2, 3 };
+            ReadOnlyCollection<int> readOnlyList = new ReadOnlyCollection<int>(list);
+            Assert.Equal(new[] { 0, 1, 2, 3 }, readOnlyList);
+            Assert.Throws<NotSupportedException>(() => readOnlyList.Remove(2));
+            Assert.Equal(new[] { 0, 1, 2, 3 }, readOnlyList);
+        }
+
+        [Fact]
+
+        public void TryingToClearTheReadonlyListShoulTrowAnError()
+        {
+            var list = new ListT<int> { 0, 1, 2, 3 };
+            ReadOnlyCollection<int> readOnlyList = new ReadOnlyCollection<int>(list);
+            Assert.Equal(new[] { 0, 1, 2, 3 }, readOnlyList);
+            Assert.Throws<NotSupportedException>(() => readOnlyList.Clear());
+            Assert.False(readOnlyList.Count == 0);
+            Assert.Equal(new[] { 0, 1, 2, 3 }, readOnlyList);
+        }
+
+        [Fact]
         public void RemoveAllElementsWithGivenValueIfListIsReadonly()
         {
             var stringArray = new ListT<string>() { "red", "red", "red" };
