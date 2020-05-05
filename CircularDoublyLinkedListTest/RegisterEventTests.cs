3b540288310a1efd1c2648019b46b85ff893af8b
CarlaCostea <CarlaCostea@users.noreply.github.com>
2020-05-05 18:05:37 +0300

    update

---
+++ CircularDoublyLinkedListTest/RegisterEventTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CircularDoublyLinkedListTest
+{
+    public class RegisterEventTests
+    {
+        [Fact]
+        public void RegisterAnEvent()
+        {
+            RegisterEvent myFirstEvent = new RegisterEvent()
+        }
+    }
+}
