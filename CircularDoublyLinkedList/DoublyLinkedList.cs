14a0c44d83afe705941882bab782eb54fc965181
CarlaCostea <CarlaCostea@users.noreply.github.com>
2020-05-08 15:36:06 +0300

    update

--- CircularDoublyLinkedList/DoublyLinkedList.cs
+++ CircularDoublyLinkedList/DoublyLinkedList.cs
@@ -7,5 +7,20 @@ namespace CircularDoublyLinkedList
     public class DoublyLinkedList
     {
         public DoublyNode Head;
+
+        public DoublyLinkedList DoubleLinkedList;
+
+        public void InsertFront(int newData)
+        {
+            DoublyNode newNode = new DoublyNode(newData);
+            newNode.Next = DoubleLinkedList.Head.Previous;
+            newNode.Previous = null;
+            if (DoubleLinkedList.Head != null)
+            {
+                DoubleLinkedList.Head.Previous = newNode.Next;
+            }
+
+            DoubleLinkedList.Head = newNode;
+        }
     }
 }
---
+++ CircularDoublyLinkedListTest/DoublyLinkedListTests.cs
@@ -0,0 +1,23 @@
+﻿using CircularDoublyLinkedList;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CircularDoublyLinkedListTest
+{
+    public class DoublyLinkedListTests
+    {
+        [Fact]
+
+        public void AddNewDataInFrontOfTheList()
+        {
+            DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
+            doublyLinkedList.InsertFront(9);
+            doublyLinkedList.InsertFront(8);
+            DoublyLinkedList secondDoublyLinkedList = new DoublyLinkedList();
+            secondDoublyLinkedList.InsertFront(8);
+            Assert.Equal(secondDoublyLinkedList.Head, doublyLinkedList.Head);
+        }
+    }
+}
